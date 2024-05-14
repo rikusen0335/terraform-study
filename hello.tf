476ad3a1c79c160832af4d77131ba4738cc349c7
Soshi Homma <rikusen0335@gmail.com>
2024-05-14 12:23:15 +0900

    add: ecs with fargate

--- hello.tf
+++ hello.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.0"
+      version = "~> 5.0"
     }
   }
 }
@@ -10,12 +10,44 @@ terraform {
 # Configure the AWS Provider
 provider "aws" {
   region = "us-east-1"
-  default_tags {
-    tags = {
-      Environment = "dev"
-      Owner       = "DevOps Team"
-    }
+  # default_tags {
+  #   tags = {
+  #     Environment = "dev"
+  #     Owner       = "DevOps Team"
+  #   }
+  # }
+}
+
+resource "aws_iam_role" "sample_iam_role" {
+  name = "sample_iam_role"
+
+  statement {
+    actions = ["ecs:RunTask"]
+    resource = ["*"]
   }
+}
+
+resource "aws_ecs_cluster" "sample_ecs_cluster" {
+  name = "sample_ecs_cluster" 
+}
+
+resource "aws_ecs_task_definition" "sample_ecs_task" {
+  family = "sample_ecs_cluster"
+  container_definitions = jsonencode([
+    {
+      name = "nginx"
+      image = "nginx:latest"
+      requires_compatibilities = ["FARGATE"]
+      cpu = 1
+      memory = 256
+      portMappings = [
+        {
+          containerPort = 80
+          hostPort = 80
+        }
+      ]
+    }
+  ])
 }
 
 # Create a VPC
