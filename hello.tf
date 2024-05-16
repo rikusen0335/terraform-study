47c1204f32d7f9276dab1406d88c625be37d3c0e
Soshi Homma <rikusen0335@gmail.com>
2024-05-16 11:07:24 +0900

    fix: syntax error of policy

--- hello.tf
+++ hello.tf
@@ -39,6 +39,7 @@ resource "aws_iam_role" "sample_iam_role" {
 resource "aws_iam_role_policy" "sample_iam_role_policy" {
   name = "sample_iam_role_policy"
   role = aws_iam_role.sample_iam_role.id
+
   policy = jsonencode({
     Version = "2012-10-17" 
     Statement = [
@@ -46,6 +47,7 @@ resource "aws_iam_role_policy" "sample_iam_role_policy" {
         Action = [
           "ecs:RunTask",
         ]
+        Effect = "Allow"
         Resource = "*"
       }
     ]
