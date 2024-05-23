84b260079269e3b793b75fc2885a05ca63869ef6
Soshi Homma <rikusen0335@gmail.com>
2024-05-23 12:13:08 +0900

    fix: name

--- hello.tf
+++ hello.tf
@@ -183,7 +183,7 @@ resource "aws_security_group" "sample_security_group" {
 }
 
 resource "aws_lb_target_group" "sample" {
-  name        = "nginx_lb"
+  name        = "nginx-lb"
   port        = 80
   protocol    = "HTTP"
   vpc_id      = aws_vpc.sample.id
