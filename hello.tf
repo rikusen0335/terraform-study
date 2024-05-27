c2c404b7d610a1a2e4dcd9bfced090c9ca824388
Soshi Homma <rikusen0335@gmail.com>
2024-05-27 10:09:26 +0900

    fix: wip

--- hello.tf
+++ hello.tf
@@ -18,6 +18,15 @@ provider "aws" {
   # }
 }
 
+module "iam_role_github_actions" {
+  source = "./modules/github_actions"
+
+  project_name = local.project_name
+  account_id   = data.aws_caller_identity.current.account_id
+  github_org   = local.github_org
+  github_repo  = local.github_repo
+}
+
 resource "aws_iam_role" "sample_iam_role" {
   name = "sample_iam_role"
 
